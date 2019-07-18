8bf3d1d39401b6cc06d777c7b9f32db608e589d2
ugurkaratash <53060055+ugurkaratash@users.noreply.github.com>
2019-07-18 23:51:02 +0300

    Add files via upload

---
+++ FJCJFKOIKH5E0E8 (1).ino
@@ -0,0 +1,150 @@
+#include  <Servo.h>
+
+Servo myservo1;
+Servo myservo2;
+Servo myservo3;
+
+void setup()
+{
+  myservo1.attach(6);
+  myservo1.write(90); // taban
+
+  myservo2.attach(9);
+  myservo2.write(60); // kol
+
+  myservo3.attach(10);
+  myservo3.write(90); //ağız
+
+}
+void loop() {
+  // A-C
+  myservo2.write(180); //kol yukarı
+  delay(1000);
+  myservo1.write(180); // Yön A
+  delay(1000);
+  myservo2.write(60); //kol aşşağı
+  delay(1000);
+  myservo3.write(180); //ağız kapalı
+  delay(1000);
+  myservo2.write(180); //kol yukarı
+  delay(1000);
+  myservo1.write(0); // Yön C
+  delay(1000);
+  myservo2.write(60); //kol aşşağı
+  delay(1000);
+  myservo3.write(90); //Ağız açık
+  delay(1000);
+  // A-B
+  myservo2.write(180); //kol yukarı
+  delay(1000);
+  myservo1.write(180); // Yön A
+  delay(1000);
+  myservo2.write(60); //kol aşşağı
+  delay(1000);
+  myservo3.write(180); //ağız kapalı
+  delay(1000);
+  myservo2.write(180); //kol yukarı
+  delay(1000);
+  myservo1.write(90); // Yön B
+  delay(1000);
+  myservo2.write(60); //kol aşşağı
+  delay(1000);
+  myservo3.write(90); //Ağız açık
+  delay(1000);
+  // C-B
+  myservo2.write(180); //kol yukarı
+  delay(1000);
+  myservo1.write(0); // Yön C
+  delay(1000);
+  myservo2.write(60); //kol aşşağı
+  delay(1000);
+  myservo3.write(180); //ağız kapalı
+  delay(1000);
+  myservo2.write(180); //kol yukarı
+  delay(1000);
+  myservo1.write(90); // Yön B
+  delay(1000);
+  myservo3.write(90); //Ağız açık
+  delay(1000);
+  // A-C
+  myservo2.write(180); //kol yukarı
+  delay(1000);
+  myservo1.write(180); // Yön A
+  delay(1000);
+  myservo2.write(60); //kol aşşağı
+  delay(1000);
+  myservo3.write(180); //ağız kapalı
+  delay(1000);
+  myservo2.write(180); //kol yukarı
+  delay(1000);
+  myservo1.write(0); // Yön C
+  delay(1000);
+  myservo2.write(60); //kol aşşağı
+  delay(1000);
+  myservo3.write(90); //Ağız açık
+  delay(1000);
+  // B-A
+  myservo2.write(180); //kol yukarı
+  delay(1000);
+  myservo1.write(90); // Yön B
+  delay(1000);
+  myservo2.write(60); //kol aşşağı
+  delay(1000);
+  myservo3.write(180); //ağız kapalı
+  delay(1000);
+  myservo2.write(180); //kol yukarı
+  delay(1000);
+  myservo1.write(180); // Yön A
+  delay(1000);
+  myservo2.write(60); //kol aşşağı
+  delay(1000);
+  myservo3.write(90); //Ağız açık
+  delay(1000);
+  // B-C
+  myservo2.write(180); //kol yukarı
+  delay(1000);
+  myservo1.write(90); // Yön B
+  delay(1000);
+  myservo2.write(60); //kol aşşağı
+  delay(1000);
+  myservo3.write(180); //ağız kapalı
+  delay(1000);
+  myservo2.write(180); //kol yukarı
+  delay(1000);
+  myservo1.write(0); // Yön C
+  delay(1000);
+  myservo2.write(60); //kol aşşağı
+  delay(1000);
+  myservo3.write(90); //Ağız açık
+  delay(1000);
+  // A-C
+  myservo2.write(180); //kol yukarı
+  delay(1000);
+  myservo1.write(180); // Yön A
+  delay(1000);
+  myservo2.write(60); //kol aşşağı
+  delay(1000);
+  myservo3.write(180); //ağız kapalı
+  delay(1000);
+  myservo2.write(180); //kol yukarı
+  delay(1000);
+  myservo1.write(0); // Yön C
+  delay(1000);
+  myservo2.write(60); //kol aşşağı
+  delay(1000);
+  myservo3.write(90); //Ağız açık
+  delay(20000);
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
